d9a834e86ffef5b27961940ec8c6674a35adf7fb
laila <laila@fake.com>
2017-12-01 22:56:33 +0200

    adding a functionality to sum the values on each row

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 
 //write a program that will check if a two dimensional arrays is magical square or not
 public class Main {
-   public static void main(String[] args) {
-       int[][] my_array = new int[][]{{1, 2, 3},
-               {5, 6, 7},
-               {6, 7, 8}};
+    public static void main(String[] args) {
+        int[][] my_array = new int[][]{{1, 2, 3},
+                {5, 6, 7},
+                {6, 7, 8}};
 
-       for (int row = 0; row < my_array.length; row++) {
-           int sumCol = 0;
-           for (int col = 0; col < my_array.length; col++) {
-               System.out.print(my_array[row][col]);
-           }
-
-           System.out.println();
-
-       }
-   }
+        for (int row = 0; row < my_array.length; row++) {
+            int sumCol = 0;
+            for (int col = 0; col < my_array.length; col++) {
+                System.out.print(my_array[row][col]);
+                sumCol+= my_array[row][col];
+            }
+            System.out.println();
+            System.out.println("Sum of row is "+ sumCol);
+        }
+    }
 
 }
