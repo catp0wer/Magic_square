dd6ecfd6ec381fdb21be559559683c203f479411
laila <laila@fake.com>
2017-12-01 23:00:28 +0200

    adding ArrayList to save the sum of each row

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,15 +8,16 @@ public class Main {
         int[][] my_array = new int[][]{{1, 2, 3},
                 {5, 6, 7},
                 {6, 7, 8}};
-
+        ArrayList<Integer> sumAllElements = new ArrayList<Integer>();
         for (int row = 0; row < my_array.length; row++) {
             int sumCol = 0;
             for (int col = 0; col < my_array.length; col++) {
                 System.out.print(my_array[row][col]);
                 sumCol+= my_array[row][col];
             }
+            sumAllElements.add(sumCol);
             System.out.println();
-            System.out.println("Sum of row is "+ sumCol);
+            System.out.println("Sums of rows are " + sumAllElements);
         }
     }
 
