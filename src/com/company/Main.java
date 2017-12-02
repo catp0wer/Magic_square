11a2112d92a3c482f01d4ea5b25aaf1a8df023ea
laila <laila@fake.com>
2017-12-02 20:28:18 +0200

    adding the functionality to save the sum for each columns

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,16 @@ import java.util.ArrayList;
 
 //write a program that will check if a two dimensional arrays is magical square or not
 public class Main {
-    public static void main(String[] args) {
-        int[][] my_array = new int[][]{{1, 2, 3},
-                {5, 6, 7},
-                {6, 7, 8}};
+    public static void main(String[] args){
+        int[][]my_array = new int[][]{ {1,2,3},
+                {5,6,7},
+                {6,7,8} };
+
         ArrayList<Integer> sumAllElements = new ArrayList<Integer>();
-        for (int row = 0; row < my_array.length; row++) {
+
+        for (int row = 0; row <my_array.length; row++){
             int sumCol = 0;
-            for (int col = 0; col < my_array.length; col++) {
+            for(int col =0;col<my_array.length;col++){
                 System.out.print(my_array[row][col]);
                 sumCol+= my_array[row][col];
             }
@@ -19,6 +21,19 @@ public class Main {
             System.out.println();
             System.out.println("Sums of rows are " + sumAllElements);
         }
+
+
+        for (int col = 0; col <my_array.length; col++){
+            int sumRow = 0;
+            for(int row =0;row<my_array.length;row++){
+                System.out.print(my_array[row][col]);
+                sumRow+= my_array[row][col];
+            }
+            sumAllElements.add(sumRow);
+            System.out.println();
+            System.out.println("Sums of rows are " + sumAllElements);
+        }
     }
+
 
 }
